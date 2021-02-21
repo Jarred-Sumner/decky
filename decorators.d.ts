89170f32df8189e0592026402348a2a31810461d
Jarred Sumner <jarred@jarredsumner.com>
2021-02-21 00:25:38 -0800

    tweak

--- decorators.d.ts
+++ decorators.d.ts
@@ -1,6 +1,9 @@
 import * as esbuild from "esbuild";
 export declare enum DecoratorType {
-    property = 0,
-    klass = 1
+  property = 0,
+  klass = 1,
 }
-export declare function decorators(decoratorGlob?: string, additionalConfig?: Partial<esbuild.BuildOptions>): Promise<string[]>;
+export declare function decorators(
+  decoratorGlob?: string,
+  additionalConfig?: Partial<esbuild.BuildOptions>
+): Promise<string[]>;
